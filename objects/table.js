b2a129095e9d122ea9e56e78f0e99bf1128693df
andylan8 <andylan9386@gmail.com>
2025-11-20 14:49:13 -0600

    Finished table and projector object

--- objects/table.js
+++ objects/table.js
@@ -4,7 +4,7 @@ such as ChatGPT, MS Copilot, other LLMs, etc.*/
 
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.164.0/build/three.module.js";
 /* Import Globals */
-import { scene, camera } from "http://127.0.0.1:5500/scene.js";
+import { scene, camera } from "../scene.js";
 
 var table = new THREE.Group();
 
